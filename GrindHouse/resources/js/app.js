76d507bc547f21ecddb278ace496bbe67365a7b5
Dineel20033 <cb013187@students.apiit.lk>
2026-02-01 23:47:25 +0530

    Remove duplicate AlpineJS initialization (Fixed Livewire conflict)

--- GrindHouse/resources/js/app.js
+++ GrindHouse/resources/js/app.js
@@ -1,7 +1,6 @@
 import './bootstrap';
 
-import Alpine from 'alpinejs';
-
-window.Alpine = Alpine;
-
-Alpine.start();
+// Alpine is already included with Livewire 3
+// import Alpine from 'alpinejs';
+// window.Alpine = Alpine;
+// Alpine.start();
